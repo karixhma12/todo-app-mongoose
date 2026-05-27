82d7dc3fb5dbef2b86d4c1f5bccae69aef6a9153
Karishma <karishma.7022@gmail.com>
2026-05-27 12:20:03 +0530

    feat : connect to MongoDB and implement signup endpoint

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {UserModel, TodoModel} = require("./db.js");
+
+mongoose.connect("mongodb+srv://karishma7022_db_user:welcome%20123@cluster0.ntuwokz.mongodb.net/todo-app");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
 
+    await UserModel.create({
+        email : email,
+        password : password,
+        name : name
+    });
+
+    res.json({message : "You are signed up!"});
 })
 
 app.post("/signin",(req,res)=>{
@@ -17,7 +31,7 @@ app.post("/todo",(req,res)=>{
 })
 
 app.get("/todos",(req,res)=>{
-    
+
 })
 
 app.listen(3000,()=>{
